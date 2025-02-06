f78d241b0399c2e98b4638e62a283302913aa363
yomna-farid <yomnafarid101@gmail.com>
2025-02-06 13:09:13 -0500

    Add project files.

---
+++ Project1-INFO5101/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Project1_INFO5101
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
