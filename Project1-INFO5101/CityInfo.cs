4b3d0fb17223787d990a81911fb96c1e28cfbe10
yomna-farid <yomnafarid101@gmail.com>
2025-02-14 15:04:44 -0500

    Json formatting and validation

--- Project1-INFO5101/CityInfo.cs
+++ Project1-INFO5101/CityInfo.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Project1_INFO5101
 {
@@ -10,10 +12,16 @@ namespace Project1_INFO5101
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [JsonProperty("state_abbrev")]
         public string StateAbbrev { get; set; }
         public string State { get; set; }
         public string Capital { get; set; }
+
+        [JsonProperty("lat")]
         public double Latitude { get; set; }
+
+        [JsonProperty("lng")]
         public double Longitude { get; set; }
         public int Population { get; set; }
         public double Density { get; set; }
